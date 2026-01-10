e14f1d960dd01d49f8607c6139bf3f47ebfbf1ac
OleguerCanal <oleguer.canal@hotmail.com>
2026-01-10 09:22:58 +0100

    tested patcher

--- src/low_budget_sora/model/video_patcher.py
+++ src/low_budget_sora/model/video_patcher.py
@@ -10,7 +10,21 @@ class VideoPatcher(nn.Module):
         N = (T/pt) * (H/ph) * (W/pw)
         patch_dim = pt * ph * pw * C
     """
-    def __init__(self, T, H, W, pt=2, ph=4, pw=4, C=1):
+    def __init__(
+            self,
+            
+            # Original
+            T: int,  # time dimension
+            H: int,  # height
+            W: int,  # width
+            
+            # Patches
+            pt: int = 2,  # time block size
+            ph: int = 4,  # height block size
+            pw: int = 4,  # width block size
+            
+            C: int = 1,  # channels
+    ):
         super().__init__()
         assert T % pt == 0 and H % ph == 0 and W % pw == 0
 
@@ -18,12 +32,13 @@ class VideoPatcher(nn.Module):
         self.pt, self.ph, self.pw = pt, ph, pw
         self.C = C
 
+        # Number of blocks
         self.t_blocks = T // pt
         self.h_blocks = H // ph
         self.w_blocks = W // pw
 
-        self.num_patches = self.t_blocks * self.h_blocks * self.w_blocks
-        self.patch_dim = pt * ph * pw * C
+        self.num_patches = self.t_blocks * self.h_blocks * self.w_blocks  # Total number of patches
+        self.patch_dim = pt * ph * pw * C  # Flattened block dimension
 
     def patchify(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -63,3 +78,33 @@ class VideoPatcher(nn.Module):
         x = x.permute(0, 1, 4, 7, 2, 5, 3, 6)  # (B, tB, pt, C, hB, ph, wB, pw)
         x = x.reshape(B, self.T, self.C, self.H, self.W)
         return x
+
+
+
+# ---- example usage ----
+if __name__ == "__main__":
+    B, T, C, H, W = 1, 16, 1, 32, 32
+    pt, ph, pw = 2, 16, 16
+    
+    # Expected number of patches
+    num_patches = (T // pt) * (H // ph) * (W // pw)
+    patch_dim = pt * ph * pw * C # Flattened block dim
+    
+    print(f"Initial block shape (T, H, W): {T, H, W}")
+    print(f"Patches distro. t: {T // pt}, h: {H // ph}, w: {W // pw}")
+    print(f"Expected patch dimension: {patch_dim}")
+    
+    x = torch.randn(B, T, C, H, W)
+    patcher = VideoPatcher(T=T, H=H, W=W, pt=pt, ph=ph, pw=pw, C=C)
+
+    # PATCHIFY
+    patches = patcher.patchify(x)
+    print(f"Patched block shape (B, N, patch_dim): {patches.shape}")
+    assert patches.shape == (B, num_patches, patch_dim)
+    
+    # UNPATCHIFY
+    unpatched = patcher.unpatchify(patches)
+    print(f"Unpatched block shape (B, T, C, H, W): {unpatched.shape}")
+    assert unpatched.shape == (B, T, C, H, W)
+    assert torch.allclose(x, unpatched)
+    print("Test passed")

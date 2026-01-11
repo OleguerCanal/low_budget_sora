d4b96b899020debe5fcb34cac5ebf35de8748226
OleguerCanal <oleguer.canal@hotmail.com>
2026-01-11 11:49:16 +0100

    Added utility paths

--- src/low_budget_sora/__init__.py
+++ src/low_budget_sora/__init__.py
@@ -2,3 +2,6 @@ import os
 
 DIR = os.path.dirname(os.path.abspath(__file__))
 COMIC_SANS_MS_FONT_PATH = os.path.join(DIR, "data", "Comic Sans MS.ttf")
+CONFIG_FILE_PATH = os.path.join(DIR, "training", "config.yaml")
+CHECKPOINTS_DIR = os.path.join(DIR, "checkpoints")
+DEBUG_GIF_PATH = os.path.join(DIR, "debug.gif")
